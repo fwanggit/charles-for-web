60bacd85cba79ff173731196e9a4152ef2da890e
tongzhichao <tongzhichao@adsage.com>
2015-12-11 13:05:51 +0800

    转向可编辑

--- public/scripts/control.js
+++ public/scripts/control.js
@@ -1,16 +1,17 @@
 
 
-var source_request_url;
-var turn_to_url;
+
 
 var responder_list=[];
+var checked=-1
 
 
 function saveclick(){
-	source_request_url= document.getElementById("source-request").value;
-	turn_to_url= document.getElementById("turn-to").value;
-	console.log("原网址:",source_request_url);
-	console.log("转向为:",turn_to_url);
+	if(checked!=-1){
+		responder_list[checked].request = document.getElementById("source-request").value;
+		responder_list[checked].turnto = document.getElementById("turn-to").value;
+	}
+	responder_refresh()
 }
 
 function inspectors_checked(){
@@ -69,7 +70,7 @@ function response_json_tab_click(){
 }
 
 function responder_add(){
-	var responder = {"id":0,"check":false,"request":"StringToMatch","turnto":""};
+	var responder = {"id":0,"position":0,"check":false,"request":"StringToMatch","turnto":""};
 	responder_list[responder_list.length]=responder;
 	responder_refresh()
 }
@@ -77,27 +78,56 @@ function responder_add(){
 function responder_refresh(){
 	var content = "";
 	for(i=0;i<responder_list.length;i++){
-		responder_list[i].id=
-		content+="<tr ><td><label><input type=\"checkbox\"  id='"+responder_list[i].id+"' onclick=\"responder_list_click("+i+",'"+responder_list[i].id+"')\">"+responder_list[i].request+"</label></td><td><label>"+responder_list[i].turnto+"</label></td> </tr>";
-console.log(responder_list[i].id);
+		if(checked==responder_list[i].position){
+			checked=i;
+		}
+		responder_list[i].position=i;
+		responder_list[i].id="respinder_list_"+i;
+		content+="<tr onclick=\"responder_list_click("+i+")\" id=\"responder_list__tr"+i+"\"><td><label><input type=\"checkbox\"  id='"+responder_list[i].id+"' onclick=\"responder_list_check("+i+")\"></label></td><td>"+responder_list[i].request+"</td><td><label>"+responder_list[i].turnto+"</label></td> </tr>";
+
 	}
+
 	document.getElementById('responder-msg').innerHTML=content;
 	for(i=0;i<responder_list.length;i++){
 		document.getElementById(responder_list[i].id).checked = responder_list[i].check;
 	}
+	if(checked!=-1){
+		document.getElementById('responder_list__tr'+checked).style.backgroundColor="#6495ED";
+	}
 }
 
-function responder_list_click(position,id){
-	responder_list[position].check = document.getElementById(id).checked;
+function responder_list_click(position){
+	if(checked==position){
+		checked=-1
+	}else{
+		checked=position;
+		refresh_form()
+	}
+
+	responder_refresh()
+
+}
+
+function refresh_form(){
+	 document.getElementById("source-request").value = responder_list[checked].request;
+	 document.getElementById("turn-to").value= responder_list[checked].turnto;
+}
+
+function responder_list_check(position){
+	responder_list[position].check = document.getElementById("respinder_list_"+position).checked;
 }
 
 function responder_clear(){
 	responder_list=[];
+	checked=-1;
 	responder_refresh()
 }
 
 function responder_delete(){
 var tmp=[];
+	if(checked!=-1&&document.getElementById(responder_list[checked].id).checked){
+		checked=-1;
+	}
 	for(i=0;i<responder_list.length;i++){
 
 if(!document.getElementById(responder_list[i].id).checked){
