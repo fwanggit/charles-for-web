819594401613fa7a3c9a9324ba736d56daeea7d3
wangfei-sal <wangfei-sal@360.cn>
2015-12-21 17:41:56 +0800

    @志超 /echo 完成

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var echo = require('./routes/echo');
 
 var app = express();
 
@@ -18,12 +19,13 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/echo', echo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/echo.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* echo server*/
+router.get('/', function(req, res, next) {
+     res.send(req.query)
+});
+router.post('/', function(req, res, next) {
+    res.send(req.body);
+});
+module.exports = router;
