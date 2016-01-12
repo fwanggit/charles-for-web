134290a906de85a9af71c8d56719ccc8769683bb
fwang <fwang_sd@126.com>
2016-01-12 22:59:21 +0800

    no message

--- proxy/charles.js
+++ proxy/charles.js
@@ -2,11 +2,17 @@
 var express = require('express');
 var router = express.Router();
 
+var redis = require('redis'); 
+var client =redis.createClient({ "host": "127.0.0.1", "port": "6379" });
 /* echo server*/
-router.get('/', function(req, res, next) {
-     res.send(req.query)
+var lists=function(req, res){
+	
+}
+
+router.get('/lists', function(req, res, next) {
+    lists(req, res);
 });
-router.post('/', function(req, res, next) {
-    res.send(req.body);
+router.post('/lists', function(req, res, next) {
+    lists(req, res);
 });
 module.exports = router;
