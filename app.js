800e098e4521277bb68e21822b2b106c429125b7
fwang <fwang_sd@126.com>
2016-01-30 10:45:52 +0800

    no message

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/echo', echo);
 app.use('/charles', require('./proxy/charles'));
-
-
+//app.use('/whiteboard', require('./routes/whiteboard'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
